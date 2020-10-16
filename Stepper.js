1695084929a2ed7c9471efa658c9c2d9de2bdf6a
Rıdvan Önal <57756211+ridvanonal@users.noreply.github.com>
2020-10-16 17:19:42 +0300

    Update Stepper.js

--- Stepper.js
+++ Stepper.js
@@ -1,9 +1,9 @@
 const counterTemplate = document.createElement("template")
 counterTemplate.innerHTML = 
 `
+<button>-</button>
 <input type="text">
 <button>+</button>
-<button>-</button>
 `
 class counter extends HTMLElement{
   constructor(){
@@ -11,6 +11,8 @@ class counter extends HTMLElement{
     this.shadow = this.attachShadow({mode:"closed"})
     this.shadow.appendChild(counterTemplate.content.cloneNode(true))
     this.countStep = 0
+    this.longSetTimeOut
+    this.longSetInterval
   }
 
   get value(){
@@ -30,7 +32,7 @@ class counter extends HTMLElement{
   }
 
   get min(){
-    return Number(this.getAttribute("min"))
+    return this.getAttribute("min")
   }
 
   set min(min){
@@ -38,29 +40,11 @@ class counter extends HTMLElement{
   }
 
   get max(){
-    return Number(this.getAttribute("max"))
+    return this.getAttribute("max")
   }
 
   set max(max){
     this.setAttribute("max",max)
-  }
-
-  add = () =>{
-    if(this.max)
-    if(this.value < this.max ) this.countStep ++
-    if(!this.max) this.countStep++
-    this.value = this.min + this.countStep * this.step 
-  }
-
-  subtract = () =>{
-    if(this.min)
-    if(this.value > this.min) this.countStep --
-    if(!this.min) this.countStep --
-    this.value = this.min + this.countStep * this.step 
-  }
-
-  maxCalculator = () =>{
-    this.max = this.min + (this.step*(Math.floor((this.max - this.min) / this.step)))
   }
 
   static get observedAttributes(){
@@ -73,6 +57,28 @@ class counter extends HTMLElement{
         this.valueChange()
       break;
     }
+  }
+
+  add = () =>{
+    if(this.max)
+    if(this.value < this.max ) this.countStep ++
+    if(!this.max) this.countStep++
+    this.valueCalculator()
+  }
+
+  subtract = () =>{
+    if(this.min)
+    if(this.value > this.min) this.countStep --
+    if(!this.min) this.countStep --
+    this.valueCalculator()
+  }
+
+  valueCalculator = () =>{
+    this.value = Number(this.min) + this.countStep * this.step 
+  }
+
+  maxCalculator = () =>{
+    this.max = Number(this.min) + (this.step*(Math.floor((this.max - Number(this.min)) / this.step)))
   }
 
   valueChange = () =>{
@@ -90,15 +96,34 @@ class counter extends HTMLElement{
     }
   }
 
+  onLongPressDown = (type) =>{
+    this.longSetTimeOut = setTimeout(()=>{
+      if(type=="add") this.longSetInterval = setInterval(()=>{this.add()},150)
+      if(type=="subtract") this.longSetInterval = setInterval(()=>{this.subtract()},150)
+    },200)
+    return false
+  }
+
+  onLongPressUp = () =>{
+    clearTimeout(this.longSetTimeOut)
+    clearInterval(this.longSetInterval)
+  }
+
   connectedCallback(){
     if(this.max) this.maxCalculator()
     if(!this.step) this.step = 1
-    if(!this.value) this.value =  this.min + this.countStep * this.step
-    let addButton = this.shadow.querySelector(":host>button:nth-of-type(1)")
-    let subtractButton = this.shadow.querySelector(":host>button:nth-of-type(2)")
+    if(!this.value) this.valueCalculator()
+    let addButton = this.shadow.querySelector(":host>button:nth-of-type(2)")
+    let subtractButton = this.shadow.querySelector(":host>button:nth-of-type(1)")
     let input = this.shadow.querySelector(":host>input")
     addButton.addEventListener("click",this.add)
+    addButton.addEventListener("mousedown",()=>this.onLongPressDown("add"))
+    addButton.addEventListener("mouseup",this.onLongPressUp)
+    addButton.addEventListener("mouseleave",this.onLongPressUp)
     subtractButton.addEventListener("click",this.subtract)
+    subtractButton.addEventListener("mousedown",()=>this.onLongPressDown("subtract"))
+    subtractButton.addEventListener("mouseup",this.onLongPressUp)
+    subtractButton.addEventListener("mouseleave",this.onLongPressUp)
     input.addEventListener("keydown",this.onKeyPress.bind(this))
   }
 
